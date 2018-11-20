4f2380830fc000164487c173a4f35a4fa3177f96
Otto Rinaldo <otto.rinaldo@gmail.com>
2018-11-20 13:35:58 +0100

    Init

---
+++ bonneTable.Services/Services/BookingService.cs
@@ -0,0 +1,32 @@
+﻿using bonneTable.Services.Interfaces;
+using bonneTalble.Models.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonneTable.Services.Services
+{
+    public class BookingService : IBookingServie
+    {
+        public Task AdminBookTable(BookingRequestModel booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AdminCancelBooking(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ClientBookTable(BookingRequestModel booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditBooking(BookingRequestModel booking, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
