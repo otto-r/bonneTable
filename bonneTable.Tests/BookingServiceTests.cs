42d740fd5dc96d8ce0f5705ef175e7c9b51bc887
rikard <rikard.hohlfalt@gmail.com>
2018-11-20 15:04:02 +0100

    Adds a few tests for BookingService

--- bonneTable.Tests/BookingServiceTests.cs
+++ bonneTable.Tests/BookingServiceTests.cs
@@ -1,3 +1,8 @@
+using AutoFixture;
+using bonneTable.Services.Interfaces;
+using bonneTable.Services.Services;
+using bonneTalble.Models.RequestModels;
+using bonneTalble.Models.ViewModels;
 using FluentAssertions;
 using System;
 using Xunit;
@@ -6,10 +11,71 @@ namespace bonneTable.Tests
 {
     public class BookingServiceTests
     {
-        [Fact]
-        public void ClientBookTable_Test()
+        private Fixture _fixture;
+
+        public BookingServiceTests()
         {
-            5.Should().Be(5);
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void ClientBookTable_WithValidValues_ReturnsSuccessViewModel()
+        {
+            var bookingService = new BookingService();
+            var bookingRequest = _fixture.Build<BookingRequestModel>().With(b => b.Seats, 2).With(b => b.Email, "wow@wow.se").Create();
+            var expected = new BookingViewModel() { Success = true};
+
+            var actual = bookingService.ClientBookTable(bookingRequest);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ClientBookTable_WithNegativeSeats_ReturnsFailureViewModel()
+        {
+            var bookingService = new BookingService();
+            var bookingRequest = _fixture.Build<BookingRequestModel>().With(b => b.Seats, -2).Create();
+            var expected = new BookingViewModel() { Success = false, ErrorMessage = "Seats are negative." };
+
+            var actual = bookingService.ClientBookTable(bookingRequest);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ClientBookTable_WithInvalidSeats_ReturnsFailureViewModel()
+        {
+            var bookingService = new BookingService();
+            var bookingRequest = _fixture.Build<BookingRequestModel>().With(b => b.Seats, 7).Create();
+            var expected = new BookingViewModel() { Success = false, ErrorMessage = "Too many seats." };
+
+            var actual = bookingService.ClientBookTable(bookingRequest);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ClientBookTable_WithEmptyName_ReturnsFailureViewModel()
+        {
+            var bookingService = new BookingService();
+            var bookingRequest = _fixture.Build<BookingRequestModel>().With(b => b.CustomerName, "").Create();
+            var expected = new BookingViewModel() { Success = false, ErrorMessage = "Customer name is empty." };
+
+            var actual = bookingService.ClientBookTable(bookingRequest);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ClientBookTable_WithBadDate_ReturnsFailureViewModel()
+        {
+            var bookingService = new BookingService();
+            var bookingRequest = _fixture.Build<BookingRequestModel>().With(b => b.Time, DateTime.Now).Create();
+            var expected = new BookingViewModel() { Success = false, ErrorMessage = "Invalid time." };
+
+            var actual = bookingService.ClientBookTable(bookingRequest);
+
+            actual.Should().BeEquivalentTo(expected);
         }
     }
 }
