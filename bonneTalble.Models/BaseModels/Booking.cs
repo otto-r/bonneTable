a2d4b725feaed2964bf95f175966dc0f878e13c2
Otto Rinaldo <otto.rinaldo@gmail.com>
2018-11-20 13:38:32 +0100

    BookingRequestModel specified

--- bonneTalble.Models/BaseModels/Booking.cs
+++ bonneTalble.Models/BaseModels/Booking.cs
@@ -12,7 +12,6 @@ namespace bonneTalble.Models
         public Table Table { get; set; }
         public string CustomerName { get; set; }
         public string PhoneNumber { get; set; }
-        public string EMail { get; set; }
-        public string MyProperty { get; set; }
+        public string Email { get; set; }
     }
 }
--- bonneTalble.Models/RequestModels/BookingRequestModel.cs
+++ bonneTalble.Models/RequestModels/BookingRequestModel.cs
@@ -6,5 +6,10 @@ namespace bonneTalble.Models.RequestModels
 {
     public class BookingRequestModel
     {
+        public DateTime Time { get; set; }
+        public int Seats { get; set; }
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
