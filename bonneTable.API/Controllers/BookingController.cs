b5dde35e150f472ce9aac9948773634348d0845d
iagohfs <iago_henriquefs@hotmail.com>
2018-11-23 12:54:20 +0100

    step 1
    Renamed ValuesController to BookingController.
    - Added a few functionality
    Added TablesController.
    - Added Methods

---
+++ bonneTable.API/Controllers/BookingController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using bonneTable.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using bonneTable.Models;
+using bonneTable.Models.RequestModels;
+
+namespace bonneTable.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        
+
+        public BookingController(IBookingService bookingServie)
+        {
+            _bookingService = bookingServie;
+            
+        }
+
+        // GET tables from requested date 
+        [HttpGet]
+        public ActionResult<IEnumerable<Booking>> Get(DateTime dateTime)
+        {
+            return _bookingService.GetBookigsByDate(dateTime);
+        }
+
+        // POST AKA ADD
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _bookingService.Add("Add this booking To The Current Date");
+        }
+
+        // PUT AKA EDIT
+        [HttpPut("{id}")]
+        public void Put(BookingRequestModel bookingValue, Guid guid)
+        {
+            _bookingService.EditBooking(bookingValue, guid);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            //_bookingService.Delete(id, DateTime???);
+        }
+    }
+}
---
+++ bonneTable.API/Controllers/TableController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bonneTable.API.Controllers
+{
+    public class TableController
+    {
+        private readonly ITableService _tableService;
+
+        public TableController(ITableService tableService)
+        {
+            _tableService = tableService;
+        }
+
+        public void Add()
+        {
+            _tableService.Add(object);
+        }
+
+        public void Delete()
+        {
+            _tableService.Delete(object);
+        }
+
+        public void Edit()
+        {
+            _tableService.Edit(object);
+        }
+
+        public void Get()
+        {
+            _tableService.Get(object);
+        }
+    }
+}
