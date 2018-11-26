853ac4af84e344cbebb11be66557cd7af767ad10
Andreas Eklund <andreas.eklund94@gmail.com>
2018-11-26 23:22:31 +0100

    updated booking repo

--- bonneTable.Services/Services/BookingRepository.cs
+++ bonneTable.Services/Services/BookingRepository.cs
@@ -1,5 +1,8 @@
-﻿using bonneTable.Models;
+﻿using bonneTable.Data;
+using bonneTable.Models;
 using bonneTable.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,49 +12,118 @@ namespace bonneTable.Services.Services
 {
     public class BookingRepository : IBookingRepository
     {
-        public Task AddAsync(Booking entity)
+        private readonly DbContext _context = null;
+
+        public BookingRepository(IOptions<Settings> settings)
         {
-            throw new NotImplementedException();
+            _context = new DbContext(settings);
         }
 
-        public Task Commit()
+        public async Task<List<Booking>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Booking.Find(_ => true).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
-        public Task Delete(Guid ID)
+        public async Task<List<Booking>> GetByDate(DateTime dateTime)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Booking.Find(b => b.Time == dateTime).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
-        public Task<bool> EditAsync(Guid ID, Booking entity)
+        public async Task<List<Booking>> GetByEmail(string email)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Booking.Find(b => b.Email == email).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
-        public Task<List<Booking>> GetAll()
+        public async Task<Booking> GetByID(Guid ID)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                return await _context.Booking.Find(b => b.Id == ID).SingleOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
 
-        public Task<List<Booking>> GetByDate(DateTime dateTime)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Booking> GetByEmail(string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Booking> GetByID(Guid ID)
-        {
-            throw new NotImplementedException();
+                throw ex;
+            }
         }
 
         Task<List<Booking>> IBookingRepository.GetByEmail(string email)
         {
             throw new NotImplementedException();
         }
+
+        public async Task AddAsync(Booking entity)
+        {
+            try
+            {
+                await _context.Booking.InsertOneAsync(entity);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Delete(Guid ID)
+        {
+            try
+            {
+                await _context.Booking.DeleteOneAsync(b => b.Id == ID);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<bool> EditAsync(Guid ID, Booking entity)
+        {
+            try
+            {
+                var filter = Builders<Booking>.Filter.Eq(t => t.Id, ID);
+                var update = Builders<Booking>.Update.Set(x => x.Seats, entity.Seats);
+
+                var updateResult = await _context.Booking.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = true });
+
+                return updateResult.IsAcknowledged && updateResult.MatchedCount > 0;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public Task Commit()
+        {
+            throw new NotImplementedException();
+        }
+               
     }
 }
