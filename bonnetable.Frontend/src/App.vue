e4629655bd8996987dc0364c40821a2a8991c78e
Unknown <ginokalyun@gmail.com>
2018-11-27 10:14:06 +0100

    Bootstrap/vue setup from school computer

--- bonnetable.Frontend/src/App.vue
+++ bonnetable.Frontend/src/App.vue
@@ -3,12 +3,12 @@
     <img src="./assets/logo.png">
     <router-view></router-view>
     <b-container class="bv-example-row">
-      <b-row>
-          <b-col>1 of 3</b-col>
-          <b-col>2 of 3</b-col>
-          <b-col>3 of 3</b-col>
-      </b-row>
-    </b-container>
+    <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+    </b-row>
+</b-container>
   </div>
 </template>
 
