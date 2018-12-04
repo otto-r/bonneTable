efaf717208d72b5124fe0fc729aea9513103b6db
Andekl <andreas.eklundit94@gmail.com>
2018-12-04 14:59:02 +0100

    Fixed a problem in TableController and added logging to TableService

--- bonneTable.Services/Services/TableService.cs
+++ bonneTable.Services/Services/TableService.cs
@@ -1,6 +1,7 @@
 ﻿using bonneTable.Models;
 using bonneTable.Models.ViewModels;
 using bonneTable.Services.Interfaces;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,6 +20,10 @@ namespace bonneTable.Services.Services
 
         public async Task<TableResponseModel> Add(int numberOfSeats)
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("C:\\test\\log.txt")
+            .CreateLogger();
             if (numberOfSeats <= 0)
             {
                 return new TableResponseModel() { ErrorMessage = "Seats cannot be less than or equal to 0", Success = false, Tables = null };
@@ -31,39 +36,60 @@ namespace bonneTable.Services.Services
             }
             catch (Exception e)
             {
+                Log.Information("Failed to add table with {Seats} seats", numberOfSeats);
                 return new TableResponseModel() { ErrorMessage = e.Message, Success = false, Tables = null };
             }
+            Log.Information("Added table with {Seats} seats", numberOfSeats);
             return new TableResponseModel() { ErrorMessage = null, Success = true, Tables = null };
         }
 
         public async Task<TableResponseModel> Delete(Guid id)
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("C:\\test\\log.txt")
+            .CreateLogger();
+
             try
             {
                 await _repository.Delete(id);
             }
             catch (Exception e)
             {
+                Log.Debug("Failed to delete table {Id}", id);
                 return new TableResponseModel() { ErrorMessage = e.Message, Success = false, Tables = null };
             }
+            Log.Information("Deleted table {Id}", id);
             return new TableResponseModel() { ErrorMessage = null, Success = true, Tables = null };
         }
 
         public async Task<TableResponseModel> Edit(Guid id, Table table)
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("C:\\test\\log.txt")
+            .CreateLogger();
+
             try
             {
                 await _repository.EditAsync(id, table);
             }
             catch (Exception e)
             {
+                Log.Information("Failed to edit table {Id}, info: {Table}", id, table);
                 return new TableResponseModel() { ErrorMessage = e.Message, Success = false, Tables = null };
             }
+            Log.Information("Editet, info: {Table}", table);
             return new TableResponseModel() { ErrorMessage = null, Success = true, Tables = null };
         }
 
         public async Task<TableResponseModel> Get(Guid id)
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("C:\\test\\log.txt")
+            .CreateLogger();
+
             Table table;
             try
             {
@@ -71,19 +97,26 @@ namespace bonneTable.Services.Services
             }
             catch (Exception e)
             {
+                Log.Debug("Tried to find a table with id {Id}. Error message: {Error}", id, e.Message);
                 return new TableResponseModel() { ErrorMessage = e.Message, Success = false, Tables = null };
             }
 
             if (table == null)
             {
+                Log.Information("Failed to find table with id {Id}", id);
                 return new TableResponseModel() { ErrorMessage = $"Unable to find table with Id {id}", Success = false, Tables = null };
             }
-
+            Log.Information("Got table with id {Id}", id);
             return new TableResponseModel() { ErrorMessage = null, Success = true, Tables = new List<Table>() { table } };
         }
 
         public async Task<TableResponseModel> Get()
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("C:\\test\\log.txt")
+            .CreateLogger();
+
             List<Table> tables;
             try
             {
@@ -91,9 +124,10 @@ namespace bonneTable.Services.Services
             }
             catch (Exception e)
             {
+                Log.Information("Failed to get all tables");
                 return new TableResponseModel() { ErrorMessage = e.Message, Success = false, Tables = null };
             }
-
+            Log.Information("Got all tables");
             return new TableResponseModel() { ErrorMessage = null, Success = true, Tables = tables };
         }
     }
