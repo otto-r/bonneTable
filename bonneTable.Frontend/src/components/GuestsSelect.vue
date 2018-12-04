ce85239d1bd7e4eb58fb4a1957c0e9914b082bc6
rikard <rikard.hohlfalt@gmail.com>
2018-12-04 15:16:34 +0100

    Now there is guests, time and confirm

--- bonneTable.Frontend/src/components/GuestsSelect.vue
+++ bonneTable.Frontend/src/components/GuestsSelect.vue
@@ -1,20 +1,32 @@
 <template>
-  <div>
-    Guests
+  <div class="mt-3">
+    <p>
+      <b-button @click="setGuests(1)" class="btn btn-lg btn-outline-primary btn-pink mr-1">1</b-button>
+      <b-button @click="setGuests(2)" class="btn btn-lg btn-outline-primary btn-pink mr-1">2</b-button>
+      <b-button @click="setGuests(3)" class="btn btn-lg btn-outline-primary btn-pink mr-1">3</b-button>
+    </p>
+    <p>
+      <b-button @click="setGuests(4)" class="btn btn-lg btn-outline-primary btn-pink mr-1">4</b-button>
+      <b-button @click="setGuests(5)" class="btn btn-lg btn-outline-primary btn-pink mr-1">5</b-button>
+      <b-button @click="setGuests(6)" class="btn btn-lg btn-outline-primary btn-pink mr-1">6</b-button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'GuestsSelect',
-  data () {
-    return {
-
+  name: "GuestsSelect",
+  data() {
+    return {};
+  },
+  methods: {
+    setGuests(guests) {
+      this.$store.commit("setGuests", guests);
+      this.$router.push({ path: "/book/time" });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
