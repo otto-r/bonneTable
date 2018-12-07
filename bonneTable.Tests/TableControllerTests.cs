2953a58f010acbfce41e33ea29c17fb6370172fa
Andekl <andreas.eklundit94@gmail.com>
2018-12-07 17:18:23 +0100

    wip

---
+++ bonneTable.Tests/TableControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using bonneTable.API.Controllers;
+using bonneTable.Services.Services;
+using NSubstitute;
+using Xunit;
+
+namespace bonneTable.Tests
+{
+    public class TableControllerTests
+    {
+        private TableService _table;
+        private Fixture _fixture;
+
+        public TableControllerTests()
+        {
+            _table = Substitute.For<TableService>();
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public async void TestGetAll()
+        {
+            var tableController = new TableController(_table);
+
+            var actual = await tableController.GetAll();
+            
+        }
+    }
+}
