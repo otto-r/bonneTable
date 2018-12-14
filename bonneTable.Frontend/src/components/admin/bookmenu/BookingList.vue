a62b3d2f7a56c37c514778fec29aaf79ba52c49d
Otto Rinaldo <otto.rinaldo@gmail.com>
2018-12-14 09:02:12 +0100

    added days names

--- bonneTable.Frontend/src/components/admin/bookmenu/BookingList.vue
+++ bonneTable.Frontend/src/components/admin/bookmenu/BookingList.vue
@@ -129,9 +129,27 @@ export default {
         month += "0";
       }
       var year = x.getFullYear();
+      var weekday = new Array(7);
+      weekday[0] = "Sunday";
+      weekday[1] = "Monday";
+      weekday[2] = "Tuesday";
+      weekday[3] = "Wednesday";
+      weekday[4] = "Thursday";
+      weekday[5] = "Friday";
+      weekday[6] = "Saturday";
       console.log(hours);
       return (
-        date + "/" + month + "/" + year + " - " + day + " " + hours + ":" + min
+        date +
+        "/" +
+        month +
+        "/" +
+        year +
+        " - " +
+        weekday[day] +
+        " " +
+        hours +
+        ":" +
+        min
       );
     }
   },
