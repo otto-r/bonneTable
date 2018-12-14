a933594dab31fc7d6696c9d5ed546ad87a0e86c2
Otto Rinaldo <otto.rinaldo@gmail.com>
2018-12-14 17:02:08 +0100

    Begun work on delete/edit modal

--- bonneTable.Frontend/src/components/admin/BookingMenu.vue
+++ bonneTable.Frontend/src/components/admin/BookingMenu.vue
@@ -1,34 +1,39 @@
 <template>
   <div>
     <h1>Booking Menu</h1>
+    <button class="btn" @click="openModal()">Show Modal</button>
     <div>
       <b-nav fill tabs>
         <b-nav-item @click="menuBook()">Book</b-nav-item>
         <b-nav-item @click="menuList()">List</b-nav-item>
       </b-nav>
     </div>
+    <EditModal v-if="displayModal" @close="displayModal = false"/>
     <BookingList v-if="displayList"></BookingList>
-    <TableAdd v-if="displayBook"></TableAdd>
   </div>
 </template>
 
 <script>
 import BookingList from "../admin/bookmenu/BookingList";
-// import --- from "../admin/bookmenu/---";
+import EditModal from "../admin/bookmenu/EditModal";
 
 export default {
   components: {
     BookingList,
-    // BookTable
+    EditModal
   },
   name: "BookingMenu",
   data() {
     return {
+      displayModal: false,
       displayList: true,
       displayBook: false
     };
   },
   methods: {
+    openModal() {
+      this.displayModal = true;
+    },
     menuBook() {
       (this.displayBook = true), (this.displayList = false);
     },
@@ -37,13 +42,13 @@ export default {
     },
     notLoggedIn() {
       if (!localStorage.loggedIn) {
-        console.log("not logged in run")
+        console.log("not logged in run");
         this.$router.push({ path: "/LogIn" });
       }
     }
   },
-  beforeCreate(){
-    this.notLoggedIn()
+  beforeCreate() {
+    this.notLoggedIn();
   }
 };
 </script>
